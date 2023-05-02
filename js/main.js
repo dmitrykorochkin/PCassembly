5c56e9ab38629189a24fac95365473a76104e8ce
Dmitry Korochkin <51984125+dmitrykorochkin@users.noreply.github.com>
2023-05-02 22:22:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ forms.forEach(form => {
   });
 });
 
-// slider 
+// slider
 const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
   spaceBetween: 70,
@@ -49,20 +49,19 @@ const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 3000
   },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    }
-  },
+  // breakpoints: {
+  //   1920: {
+  //     slidesPerView: 3
+  //   },
+  //   320: {
+  //     slidesPerView: 1
+  //   }
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   }
 });
-
-
-
 
 //accordion
 
@@ -71,6 +70,8 @@ let tab = document.querySelectorAll(".answerblock__item");
 let answer = document.querySelectorAll(".answerblock__item-text");
 let plus = document.querySelectorAll(".answerblock__icon");
 let close = document.querySelectorAll(".answerblock__icon-close");
+let title = document.querySelectorAll(".answerblock__item-title ");
+
 
 accordion.addEventListener("click", e => {
   const target = e.target.closest(".answerblock__item");
@@ -79,11 +80,13 @@ accordion.addEventListener("click", e => {
     tab.forEach((item, index) => {
       if (item === target && target.classList.toggle("active")) {
         answer[index].classList.add("active");
+        title[index].classList.add("active-item");
         tab[index].classList.add("answerblock__item-active");
         plus[index].style.display = "none";
         close[index].style.display = "block";
       } else {
         answer[index].classList.remove("active");
+         title[index].classList.remove("active-item");
         tab[index].classList.remove("answerblock__item-active");
         plus[index].style.display = "block";
         close[index].style.display = "none";
@@ -92,8 +95,7 @@ accordion.addEventListener("click", e => {
   }
 });
 
-
-// tabs 
+// tabs
 
 const itemLeft = document.querySelectorAll(".shooting__video-item");
 const img = document.querySelectorAll(".shooting__video-play");
@@ -118,16 +120,13 @@ itemTop.forEach((item, index) => {
       i.classList.remove("active");
     });
     imgBlock[index].classList.add("active");
-
   });
 });
 
-// tabs интерфейс 
-
+// tabs интерфейс
 
 const itemIntLeft = document.querySelectorAll(".interface__item");
 const imgIntBlock = document.querySelectorAll(".interface__img");
-
 
 itemIntLeft.forEach((item, index) => {
   item.addEventListener("mouseover", () => {
@@ -138,10 +137,10 @@ itemIntLeft.forEach((item, index) => {
   });
 });
 
-// alert 
+// alert
 
-const alertTabs = document.querySelectorAll('.alert__tab');
-const alertImgBlock = document.querySelectorAll('.alert__animation');
+const alertTabs = document.querySelectorAll(".alert__tab");
+const alertImgBlock = document.querySelectorAll(".alert__animation");
 
 alertTabs.forEach((item, index) => {
   item.addEventListener("mouseover", () => {
@@ -149,12 +148,10 @@ alertTabs.forEach((item, index) => {
       item.classList.remove("active");
     });
     alertImgBlock[index].classList.add("active");
-    
   });
 });
 
-
-//hover доставка и ответы 
+//hover доставка и ответы
 
 const itemHover = document.querySelectorAll(".answer__pay");
 const sectionHover = document.querySelectorAll(".section__hover");
@@ -168,30 +165,84 @@ itemHover.forEach((item, index) => {
   });
 });
 
-
-// переключатель checkbox 
+// переключатель checkbox
 
 const toggleCheckbox = document.querySelector('input[type="checkbox"]');
 const briefly = document.querySelector(".charachterisitc__wrapper");
 const detail = document.querySelector(".elevanth__tabl-wrapper");
 
-
-toggleCheckbox.addEventListener('change', () => {
-  if(toggleCheckbox.checked) {
-    briefly.style.display = 'none';
+toggleCheckbox.addEventListener("change", () => {
+  if (toggleCheckbox.checked) {
+    briefly.style.display = "none";
     detail.style.display = "block";
   } else {
-      briefly.style.display = "block";
-      detail.style.display = "none";
+    briefly.style.display = "block";
+    detail.style.display = "none";
   }
-})
-
-
-// бургер меню 
-
-const navbarToggle = document.getElementById("navbar__toggle");
-const navbarMenu = document.getElementById("navbar");
-
-navbarToggle.addEventListener("click", () => {
-  navbarMenu.classList.toggle("active");
 });
+
+// бургер меню
+
+const navbarMenu = document.querySelector(".navbar");
+const buttonBurger = document.querySelector(".header__burger");
+const buttonBurgerClose = document.querySelector(".header__burger-close");
+
+buttonBurger.addEventListener("click", () => {
+  navbarMenu.classList.add("navbar__visible");
+  buttonBurger.style.display = "none";
+  buttonBurgerClose.style.display = "block";
+});
+
+buttonBurgerClose.addEventListener("click", () => {
+  navbarMenu.classList.remove("navbar__visible");
+  buttonBurger.style.display = "block";
+  buttonBurgerClose.style.display = "none";
+});
+
+// загрузка страниц при скролле
+
+let page = 1;
+let perPage = 8;
+
+function loadMoreData() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "load_data.php", true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      //добавляем новые данные в контейнер
+      document.getElementById("data-container").innerHTML += xhr.responseText;
+
+      page++;
+    }
+  };
+  xhr.send("page=" + page + "&perPage" + perPage);
+}
+window.onscroll = function() {
+  // Вычисляем высоту страницы и расстояние до конца страницы
+  var scrollTop =
+    window.pageYOffset !== undefined
+      ? window.pageYOffset
+      : (document.documentElement || document.body.parentNode || document.body)
+          .scrollTop;
+  var windowHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+  var documentHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+
+  var bottomOffset = documentHeight - (scrollTop + windowHeight);
+
+  // Если расстояние до конца страницы меньше 100 пикселей, загружаем новые данные
+  if (bottomOffset < 100) {
+    loadMoreData();
+  }
+};
+
+
