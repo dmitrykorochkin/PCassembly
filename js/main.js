6da6939dab001d5e1cdc4347567ce43306cf99b4
Dmitry Korochkin <51984125+dmitrykorochkin@users.noreply.github.com>
2023-05-04 20:39:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -246,3 +246,33 @@ window.onscroll = function() {
 };
 
 
+  // == Custom cursor ==========
+  const cursor = document.querySelector(".coursor");
+
+  document.addEventListener("mousemove", function (e) {
+    cursor.style.cssText =
+      "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+  });
+
+  document.addEventListener("mouseover", function (e) {
+    if (e.target.closest("button, a")) {
+      cursor.classList.add("_over");
+    }
+  });
+
+  document.addEventListener("mouseout", function (e) {
+    if (e.target.closest("button, a")) {
+      cursor.classList.remove("_over");
+    }
+  });
+
+  document.addEventListener("mousedown", function (e) {
+    if (e.target.closest("button, a")) {
+      cursor.classList.add("click");
+      cursor.classList.remove("_over");
+      setTimeout(function () {
+        cursor.classList.remove("click");
+        cursor.classList.add("_over");
+      }, 500);
+    }
+  });
